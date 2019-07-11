4cbadd0746cf2e0bf548aba246dd63d219ac58fc
JP Cimalando <jp-dev@inbox.ru>
2019-07-11 02:24:26 +0200

    optimize the png

--- plugins/string-machine/Artwork.hpp
+++ plugins/string-machine/Artwork.hpp
@@ -12,22 +12,22 @@ namespace Artwork
     const unsigned int ProggySquareDataSize = 41588;
 
     extern const char* knobData;
-    const unsigned int knobDataSize = 9416;
+    const unsigned int knobDataSize = 8079;
 
     extern const char* ledData;
     const unsigned int ledDataSize = 199;
 
     extern const char* sliderData;
-    const unsigned int sliderDataSize = 11348;
+    const unsigned int sliderDataSize = 9927;
 
     extern const char* toggle_buttonData;
-    const unsigned int toggle_buttonDataSize = 844;
+    const unsigned int toggle_buttonDataSize = 837;
 
     extern const char* value_displayData;
-    const unsigned int value_displayDataSize = 134;
+    const unsigned int value_displayDataSize = 131;
 
     extern const char* vuData;
-    const unsigned int vuDataSize = 4899;
+    const unsigned int vuDataSize = 3443;
 }
 
 #endif // BINARY_ARTWORK_HPP
