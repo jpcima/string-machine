44af4f89e33a6371b75de5ed8cae940ba56b31e9
JP Cimalando <jp-dev@inbox.ru>
2019-07-18 01:52:50 +0200

    eliminate warnings in the fontstash build

--- sources/ui/Fontstash.cpp
+++ sources/ui/Fontstash.cpp
@@ -1,2 +1,7 @@
+// disable an avalanche of "unused function" warnings
+#if defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wunused-function"
+#endif
+
 #define FONTSTASH_IMPLEMENTATION
 #include <fontstash.h>
